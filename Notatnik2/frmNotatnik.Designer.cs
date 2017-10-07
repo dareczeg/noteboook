55e3b7a03a0bf5f64d00e67fe29fcd115b6fceea
dareczeg <32062687+dareczeg@users.noreply.github.com>
2017-10-07 21:40:48 +0200

    Add files via upload

---
+++ Notatnik2/frmNotatnik.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Notatnik
+{
+    partial class frmNotatnik
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otwórzToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszJakoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatujToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zmieńTłoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatujToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.autorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtTresc = new System.Windows.Forms.TextBox();
+            this.fontDialog = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.formatujToolStripMenuItem,
+            this.autorToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(484, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowyToolStripMenuItem,
+            this.otwórzToolStripMenuItem,
+            this.zapiszToolStripMenuItem,
+            this.zapiszJakoToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // nowyToolStripMenuItem
+            // 
+            this.nowyToolStripMenuItem.Name = "nowyToolStripMenuItem";
+            this.nowyToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.nowyToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.nowyToolStripMenuItem.Text = "Nowy";
+            this.nowyToolStripMenuItem.Click += new System.EventHandler(this.nowyToolStripMenuItem_Click);
+            // 
+            // otwórzToolStripMenuItem
+            // 
+            this.otwórzToolStripMenuItem.Name = "otwórzToolStripMenuItem";
+            this.otwórzToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.otwórzToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.otwórzToolStripMenuItem.Text = "Otwórz";
+            this.otwórzToolStripMenuItem.Click += new System.EventHandler(this.otwórzToolStripMenuItem_Click);
+            // 
+            // zapiszToolStripMenuItem
+            // 
+            this.zapiszToolStripMenuItem.Name = "zapiszToolStripMenuItem";
+            this.zapiszToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.zapiszToolStripMenuItem.Text = "Zapisz";
+            this.zapiszToolStripMenuItem.Click += new System.EventHandler(this.zapiszToolStripMenuItem_Click);
+            // 
+            // zapiszJakoToolStripMenuItem
+            // 
+            this.zapiszJakoToolStripMenuItem.Name = "zapiszJakoToolStripMenuItem";
+            this.zapiszJakoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.zapiszJakoToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.zapiszJakoToolStripMenuItem.Text = "Zapisz jako";
+            this.zapiszJakoToolStripMenuItem.Click += new System.EventHandler(this.zapiszJakoToolStripMenuItem_Click);
+            // 
+            // formatujToolStripMenuItem
+            // 
+            this.formatujToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zmieńTłoToolStripMenuItem,
+            this.formatujToolStripMenuItem1});
+            this.formatujToolStripMenuItem.Name = "formatujToolStripMenuItem";
+            this.formatujToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.formatujToolStripMenuItem.Text = "Edytuj";
+            this.formatujToolStripMenuItem.Click += new System.EventHandler(this.formatujToolStripMenuItem_Click);
+            // 
+            // zmieńTłoToolStripMenuItem
+            // 
+            this.zmieńTłoToolStripMenuItem.Name = "zmieńTłoToolStripMenuItem";
+            this.zmieńTłoToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.zmieńTłoToolStripMenuItem.Text = "Zmień tło";
+            this.zmieńTłoToolStripMenuItem.Click += new System.EventHandler(this.zmieńTłoToolStripMenuItem_Click);
+            // 
+            // formatujToolStripMenuItem1
+            // 
+            this.formatujToolStripMenuItem1.Name = "formatujToolStripMenuItem1";
+            this.formatujToolStripMenuItem1.Size = new System.Drawing.Size(125, 22);
+            this.formatujToolStripMenuItem1.Text = "Formatuj";
+            this.formatujToolStripMenuItem1.Click += new System.EventHandler(this.formatujToolStripMenuItem1_Click);
+            // 
+            // autorToolStripMenuItem
+            // 
+            this.autorToolStripMenuItem.Name = "autorToolStripMenuItem";
+            this.autorToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.autorToolStripMenuItem.Text = "Autor";
+            this.autorToolStripMenuItem.Click += new System.EventHandler(this.autorToolStripMenuItem_Click);
+            // 
+            // txtTresc
+            // 
+            this.txtTresc.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txtTresc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtTresc.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtTresc.Location = new System.Drawing.Point(0, 24);
+            this.txtTresc.Multiline = true;
+            this.txtTresc.Name = "txtTresc";
+            this.txtTresc.Size = new System.Drawing.Size(484, 237);
+            this.txtTresc.TabIndex = 1;
+            this.txtTresc.TextChanged += new System.EventHandler(this.txtTresc_TextChanged);
+            // 
+            // frmNotatnik
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.txtTresc);
+            this.Controls.Add(this.menu);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.HelpButton = true;
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menu;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmNotatnik";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Notatnik";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nowyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otwórzToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zapiszToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zapiszJakoToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtTresc;
+        private System.Windows.Forms.ToolStripMenuItem formatujToolStripMenuItem;
+        private System.Windows.Forms.FontDialog fontDialog;
+        private System.Windows.Forms.ToolStripMenuItem autorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zmieńTłoToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStripMenuItem formatujToolStripMenuItem1;
+    }
+}
+
---
+++ Notatnik2/frmNotatnik.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Notatnik
+{
+    public partial class frmNotatnik : Form
+    {
+        public frmNotatnik()
+        {
+            InitializeComponent();
+        }
+        string plik = "";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nowyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (txtTresc.Text != "")
+            {
+                DialogResult odp = czyzapisac();
+                if (odp == DialogResult.Cancel)
+                    return;
+                plik = "";
+                txtTresc.Clear();
+            }
+        }
+        private DialogResult czyzapisac()
+        {
+            DialogResult odp = MessageBox.Show("Chcesz zapisać zmiany?", "Notatnik",
+            MessageBoxButtons.YesNoCancel, MessageBoxIcon.Exclamation);
+            if (odp == DialogResult.Yes)
+                zapiszToolStripMenuItem_Click(null, null);
+            return odp;
+        }
+
+        private void otwórzToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (txtTresc.Text != "")
+            {
+                DialogResult odp = czyzapisac();
+                if (odp == DialogResult.Cancel)
+                    return;
+                plik = "";
+                txtTresc.Clear();
+            }
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Plik tekstowy (*.txt)|*.txt";
+            dialog.Multiselect = false;
+            dialog.ShowDialog();
+            if (dialog.FileName != "")
+            {
+                plik = dialog.FileName;
+                StreamReader f = new StreamReader(plik);
+                txtTresc.Text = f.ReadToEnd();
+                f.Close();
+            }
+        }
+
+        private void zapiszToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (plik != "")
+            {
+                StreamWriter f = new StreamWriter(plik);
+                f.Write(txtTresc.Text);
+                f.Close();
+            }
+            else zapiszJakoToolStripMenuItem_Click(sender, e);
+        }
+
+        private void zapiszJakoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Plik tekstowy (*.txt)|*.txt";
+            dialog.ShowDialog();
+            if (dialog.FileName != "")
+            {
+                plik = dialog.FileName;
+                StreamWriter f = new StreamWriter(plik);
+                f.Write(txtTresc.Text);
+                f.Close();
+            }
+        }
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (txtTresc.Text != "")
+            {
+                DialogResult odp = czyzapisac();
+                if (odp == DialogResult.Cancel)
+                    e.Cancel = true;
+            }
+        }
+       
+       
+
+        private void txtTresc_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formatujToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+          
+           
+        }
+
+        private void autorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+         MessageBox.Show("Dariusz Ghaemi 2017 inspired by CentrumXP.pl");
+        }
+
+        private void zmieńTłoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtTresc.BackColor = colorDialog1.Color; 
+            }
+        }
+
+        private void formatujToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fontDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Font font = fontDialog.Font;
+                this.txtTresc.Text = string.Format("");
+                this.txtTresc.Font = font;
+            }
+        }
+
+       
+       
+    }
+}
